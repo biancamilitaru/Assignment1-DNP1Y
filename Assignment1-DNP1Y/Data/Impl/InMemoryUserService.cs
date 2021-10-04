744f8a08cb0b0040d0f740d383d6a81288c41315
kyratolnai11 <266375@viauc.dk>
2021-10-04 11:29:37 +0200

    developing functonalities for login

---
+++ Assignment1-DNP1Y/Data/Impl/InMemoryUserService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Models;
+
+namespace Assignment1_DNP1Y.Data.Impl
+{
+    public class InMemoryUserService: IUserInterface
+    {
+        private List<User> users;
+
+        public InMemoryUserService()
+        {
+            users = new[]
+            {
+                new User
+                {
+                    UserName = "biancaMilitaru",
+                    Password = "12345"
+                },
+                new User()
+                {
+                    UserName = "milanTolnai",
+                    Password = "54321"
+                }
+            }.ToList();
+        }
+        
+        
+        public User ValidateUser(string userName, string password)
+        {
+            User first = users.FirstOrDefault(user => user.UserName.Equals(userName));
+            if (first == null)
+            {
+                throw new Exception("User not found");
+            }
+
+            if (!first.Password.Equals(password))
+            {
+                throw new Exception("Incorrect password");
+            }
+
+            return first;
+        }
+    }
+}
--- Assignment1-DNP1Y/Startup.cs
+++ Assignment1-DNP1Y/Startup.cs
@@ -1,10 +1,15 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
+using Assignment1_DNP1Y.Authentication;
 using Assignment1_DNP1Y.Data;
+using Assignment1_DNP1Y.Data.Impl;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
@@ -30,32 +35,39 @@ namespace Assignment1_DNP1Y
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<IAdultsData, AdultsJSONData>();
+            services.AddScoped<IUserInterface, InMemoryUserService>();
+            services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
+            services.AddAuthorization(options =>
+            {
+               
+                options.AddPolicy("MustBeVIA", a => a.RequireAuthenticatedUser().RequireClaim("Password","1"));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
+            public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
             {
-                app.UseDeveloperExceptionPage();
+                if (env.IsDevelopment())
+                {
+                    app.UseDeveloperExceptionPage();
+                }
+                else
+                {
+                    app.UseExceptionHandler("/Error");
+                    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                    app.UseHsts();
+                }
+
+                app.UseHttpsRedirection();
+                app.UseStaticFiles();
+
+                app.UseRouting();
+
+                app.UseEndpoints(endpoints =>
+                {
+                    endpoints.MapBlazorHub();
+                    endpoints.MapFallbackToPage("/_Host");
+                });
             }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapBlazorHub();
-                endpoints.MapFallbackToPage("/_Host");
-            });
         }
     }
-}
